0bb52064435f3a1734a83e0410f6ab99d7cce913
Guilherme-VMonteiro <guilhermevmonteiro2512@gmail.com>
2024-05-29 11:48:28 -0300

    att

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,22 +4,39 @@ import Wordle from "../Wordle/Wordle";
 const App = () => {
    const [solution, setSolution] = useState(null);
 
+   // useEffect(() => {
+   //    fetch("http://localhost:3001/solutions")
+   //       .then((res) => res.json())
+   //       .then((json) => {
+   //          const randomSolution =
+   //             json[Math.floor(Math.random() * json.length)];
+   //          setSolution(randomSolution.word);
+   //       });
+   // }, [setSolution]);
+
    useEffect(() => {
-      fetch("http://localhost:3001/solutions")
-         .then((res) => res.json())
-         .then((json) => {
-            const randomSolution =
-               json[Math.floor(Math.random() * json.length)];
-            setSolution(randomSolution.word);
-         });
+      const solutions = [
+         { id: 1, word: "bojas" },
+         { id: 2, word: "guiga" },
+         { id: 3, word: "bomba" },
+         { id: 4, word: "gamer" },
+         { id: 5, word: "corki" },
+         { id: 6, word: "tetas" },
+         { id: 7, word: "termo" },
+         { id: 8, word: "nuget" },
+         { id: 9, word: "soloq" },
+      ];
+
+      const randomSolution = solutions[Math.floor(Math.random() * solutions.length)];
+      setSolution(randomSolution.word);
    }, [setSolution]);
 
    return (
-    <div className="bg-slate-800 w-screen h-screen flex flex-col items-center justify-center text-white">
-        <h1 className="text-4xl py-4 uppercase font-bold">Termo da silva</h1>
-        {solution && <Wordle solution={solution}/>}
-    </div>
-    )
+      <div className="bg-slate-800 w-screen h-screen flex flex-col items-center justify-center text-white">
+         <h1 className="text-4xl py-4 uppercase font-bold">Termo da silva</h1>
+         {solution && <Wordle solution={solution} />}
+      </div>
+   );
 };
 
 export default App;
--- src/components/Keypad/Keypad.jsx
+++ src/components/Keypad/Keypad.jsx
@@ -3,12 +3,44 @@ import React, { useEffect, useState } from "react";
 const Keypad = ({ usedKeys }) => {
    const [letters, setLetters] = useState(null);
 
+   // useEffect(() => {
+   //    fetch("http://localhost:3001/letters")
+   //       .then((res) => res.json())
+   //       .then((json) => {
+   //          setLetters(json);
+   //       });
+   // }, []);
+
    useEffect(() => {
-      fetch("http://localhost:3001/letters")
-         .then((res) => res.json())
-         .then((json) => {
-            setLetters(json);
-         });
+      const letters = [
+         { key: "a" },
+         { key: "b" },
+         { key: "c" },
+         { key: "d" },
+         { key: "e" },
+         { key: "f" },
+         { key: "g" },
+         { key: "h" },
+         { key: "i" },
+         { key: "j" },
+         { key: "k" },
+         { key: "l" },
+         { key: "m" },
+         { key: "n" },
+         { key: "o" },
+         { key: "p" },
+         { key: "q" },
+         { key: "r" },
+         { key: "s" },
+         { key: "t" },
+         { key: "u" },
+         { key: "v" },
+         { key: "w" },
+         { key: "x" },
+         { key: "y" },
+         { key: "z" },
+      ];
+      setLetters(letters)
    }, []);
 
    return (
@@ -19,8 +51,9 @@ const Keypad = ({ usedKeys }) => {
                return (
                   <div
                      key={letter.key}
-                     className={`m-1 w-10 h-12 inline-block rounded-md leading-[50px] ${color === undefined ? "bg-slate-700": color}`
-                     }
+                     className={`m-1 w-10 h-12 inline-block rounded-md leading-[50px] ${
+                        color === undefined ? "bg-slate-700" : color
+                     }`}
                   >
                      {letter.key}
                   </div>
